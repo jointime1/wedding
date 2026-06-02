abd136e561c4c9d5f3b9aeea848ce6f5ac081716
Alexander Starodubtsev <96395085+jointime1@users.noreply.github.com>
2026-06-02 13:51:03 +0300

    Add GitLab Pages CI; point site at gitlab.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://jointime1.github.io',
+  site: 'https://jointime.alex.gitlab.io',
   base: '/wedding',
   output: 'static',
   trailingSlash: 'ignore',
