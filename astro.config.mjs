cf3a9dd455cf941c84bcccb34837e69c7f5e64c0
Alexander Starodubtsev <96395085+jointime1@users.noreply.github.com>
2026-06-02 13:53:01 +0300

    Serve at Pages root (unique domain): base=/

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://jointime.alex.gitlab.io',
-  base: '/wedding',
+  site: 'https://wedding-a38614.gitlab.io',
+  base: '/',
   output: 'static',
   trailingSlash: 'ignore',
   integrations: [tailwind()],
